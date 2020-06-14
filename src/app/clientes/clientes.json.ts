cddd470c2d7d6373d26c9025b8bf4ff3b83cb41a
Carolina Ponce <lapaisacarito@gmail.com>
2020-06-14 10:52:53 -0500

    lista de clientes

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,12 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[]= [
+    {id:1,nombre:'Carolina', apellido:'Ponce',email:'lapaisacarito@gmail.com',createAt:'2020-06-14'},
+    {id:1,nombre:'Ana Sofia', apellido:'Young',email:'lapaisacarito2@gmail.com',createAt:'2020-06-14'},
+    {id:1,nombre:'Johnny', apellido:'Young',email:'lapaisacarito3@gmail.com',createAt:'2020-06-14'},
+    {id:1,nombre:'Dolly', apellido:'Marin',email:'lapaisacarito4@gmail.com',createAt:'2020-06-14'},
+    {id:1,nombre:'Amparo', apellido:'Marin',email:'lapaisacarito5@gmail.com',createAt:'2020-06-14'},
+    {id:1,nombre:'Lucero', apellido:'Zapata',email:'lapaisacarito6@gmail.com',createAt:'2020-06-14'},
+    {id:1,nombre:'Mery', apellido:'Zapata',email:'lapaisacarito7@gmail.com',createAt:'2020-06-14'},
+    {id:1,nombre:'John Jairo', apellido:'Zapata',email:'lapaisacarito8@gmail.com',createAt:'2020-06-14'}
+  ];
