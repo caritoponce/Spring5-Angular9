7327fd9a43762ef0bae20beba114bfd17535f997
Carolina Ponce <lapaisacarito@gmail.com>
2020-06-14 11:27:37 -0500

    Creacion de servicio

--- src/app/clientes/clientes.json.ts
+++ src/app/clientes/clientes.json.ts
@@ -2,11 +2,11 @@ import { Cliente } from './cliente';
 
 export const CLIENTES: Cliente[]= [
     {id:1,nombre:'Carolina', apellido:'Ponce',email:'lapaisacarito@gmail.com',createAt:'2020-06-14'},
-    {id:1,nombre:'Ana Sofia', apellido:'Young',email:'lapaisacarito2@gmail.com',createAt:'2020-06-14'},
-    {id:1,nombre:'Johnny', apellido:'Young',email:'lapaisacarito3@gmail.com',createAt:'2020-06-14'},
-    {id:1,nombre:'Dolly', apellido:'Marin',email:'lapaisacarito4@gmail.com',createAt:'2020-06-14'},
-    {id:1,nombre:'Amparo', apellido:'Marin',email:'lapaisacarito5@gmail.com',createAt:'2020-06-14'},
-    {id:1,nombre:'Lucero', apellido:'Zapata',email:'lapaisacarito6@gmail.com',createAt:'2020-06-14'},
-    {id:1,nombre:'Mery', apellido:'Zapata',email:'lapaisacarito7@gmail.com',createAt:'2020-06-14'},
-    {id:1,nombre:'John Jairo', apellido:'Zapata',email:'lapaisacarito8@gmail.com',createAt:'2020-06-14'}
+    {id:2,nombre:'Ana Sofia', apellido:'Young',email:'lapaisacarito2@gmail.com',createAt:'2020-06-14'},
+    {id:3,nombre:'Johnny', apellido:'Young',email:'lapaisacarito3@gmail.com',createAt:'2020-06-14'},
+    {id:4,nombre:'Dolly', apellido:'Marin',email:'lapaisacarito4@gmail.com',createAt:'2020-06-14'},
+    {id:5,nombre:'Amparo', apellido:'Marin',email:'lapaisacarito5@gmail.com',createAt:'2020-06-14'},
+    {id:6,nombre:'Lucero', apellido:'Zapata',email:'lapaisacarito6@gmail.com',createAt:'2020-06-14'},
+    {id:7,nombre:'Mery', apellido:'Zapata',email:'lapaisacarito7@gmail.com',createAt:'2020-06-14'},
+    {id:8,nombre:'John Jairo', apellido:'Zapata',email:'lapaisacarito8@gmail.com',createAt:'2020-06-14'}
   ];
