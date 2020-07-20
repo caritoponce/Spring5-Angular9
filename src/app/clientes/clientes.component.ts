691b039a72e3fbe0a80fa4cb3531037253c91ec3
Carolina Ponce <lapaisacarito@gmail.com>
2020-07-20 15:33:04 -0500

    Crud con api spring boot java get-post-put-delete

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
+import Swal from 'sweetalert2';
 
 
 @Component({
@@ -17,6 +18,43 @@ export class ClientesComponent implements OnInit {
     this.clienteService.getClientes().subscribe(
      clientes =>  this.clientes= clientes
     );
+  }
+
+  delete(cliente: Cliente): void{
+
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: 'btn btn-success',
+        cancelButton: 'btn btn-danger'
+      },
+      buttonsStyling: false
+    })
+    
+    swalWithBootstrapButtons.fire({
+      title: 'Está seguro?',
+      text: `Seguro que dese eliminar al cliente ${cliente.nombre}  ${cliente.apellido} ?`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Si, eliminar!',
+      cancelButtonText: 'No, cancelar!',
+      reverseButtons: true
+    }).then((result) => {
+      if (result.value) {
+
+        this.clienteService.delete(cliente.id).subscribe(
+          response =>{
+            this.clientes = this.clientes.filter(cli =>  cli !== cliente)
+            swalWithBootstrapButtons.fire(
+            
+              'Cliente Eliminado!',
+              `Cliente ${cliente.nombre} eliminado con éxito.`,
+              'success'
+            )
+          }
+        )
+      }
+    })
+
   }
 
 }
--- src/app/clientes/form.component.ts
+++ src/app/clientes/form.component.ts
@@ -1,26 +1,48 @@
 import { Component, OnInit } from '@angular/core';
-import {Cliente} from './cliente'
+import { Cliente } from './cliente'
 import { ClienteService } from './cliente.service';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute} from '@angular/router';
+import Swal from 'sweetalert2';
 
 
 @Component({
   selector: 'app-form',
-  templateUrl: './form.component.html' 
+  templateUrl: './form.component.html'
 })
 export class FormComponent implements OnInit {
 
-  public cliente : Cliente = new Cliente()
-  public titulo: string ="Crear Cliente"
-  constructor(private clienteService:ClienteService, private router:Router ) { }
+  public cliente: Cliente = new Cliente()
+  public titulo: string = "Crear Cliente"
+  constructor(private clienteService: ClienteService, 
+    private router: Router, 
+    private activateRoute:ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.cargarCliente()
   }
 
-  public create():void{
-   this.clienteService.create(this.cliente).subscribe(
-     response => this.router.navigate(['/clientes'])
-   )
+  cargarCliente(): void{
+    this.activateRoute.params.subscribe(params=> {
+      let id= params['id']
+      if(id){
+        this.clienteService.getCliente(id).subscribe( (cliente) => this.cliente = cliente)
+      }
+    })
+  }
+  public create(): void {
+    this.clienteService.create(this.cliente).subscribe(cliente => {
+      this.router.navigate(['/clientes'])
+      Swal.fire('Nuevo Cliente', `Cliente ${cliente.nombre} creado con éxito!`, 'success')
+    });
+
+  }
+
+  update(): void{
+    this.clienteService.update(this.cliente).
+    subscribe(cliente=> {
+      this.router.navigate(['/clientes'])
+      Swal.fire('Cliente Actualizado', `Cliente ${cliente.nombre} actualizado con éxito!`, 'success')
+    });
   }
 
 
